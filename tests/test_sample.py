20d4a4bb03fdadb7ae4c85dedfeab002ea8639a4
Zhaowei <zwfangio@hotmail.com>
2018-12-17 11:30:05 +0800

    structure project

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -5,7 +5,7 @@ from serendipity import sample
 class TestSample(unittest.TestCase):
     def test_sample(self):
         print("hello, world")
-        assert sample.sample() == 10
+        self.assertEqual(sample.sample(), 10)
 
 
 if __name__ == "__main__":
