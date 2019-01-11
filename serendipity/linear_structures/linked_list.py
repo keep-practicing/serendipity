eb182cc6b260bc71c155aa4fdade012447dcc7c4
Zhaowei <33212232+zwfang@users.noreply.github.com>
2019-01-11 11:07:23 +0800

    single linked list (#6)
    * single linked list
    
    * update readme

---
+++ serendipity/linear_structures/linked_list.py
@@ -0,0 +1,99 @@
+class IllegalArgumentException(Exception):
+    pass
+
+
+class LinkedList:
+    class _Node:
+        def __init__(self, val=None, next=None):
+            self.val = val
+            self.next = next
+        
+        # def __str__(self):
+        #     return str(self.val)
+        
+        # def __repr__(self):
+        #     return self.__str__()
+
+    def __init__(self):
+        self._dummy_head = self._Node()
+        self._size = 0
+
+    def get_size(self):
+        return self._size
+
+    def is_empty(self):
+        return self._size == 0
+
+    def add(self, index, e):
+        if index < 0 or index > self._size:
+            raise IllegalArgumentException("add failed, illegal index.")
+        prev = self._dummy_head
+        for _ in range(index):
+            prev = prev.next
+        prev.next = self._Node(e, prev.next)
+        self._size += 1
+
+    def add_first(self, e):
+        self.add(0, e)
+
+    def add_last(self, e):
+        self.add(self._size, e)
+
+    def get(self, index):
+        if index < 0 or index >= self._size:
+            raise IllegalArgumentException("get failed, illegal index.")
+        cur = self._dummy_head.next
+        for _ in range(index):
+            cur = cur.next
+        return cur.val
+
+    def get_first(self):
+        return self.get(0)
+
+    def get_last(self):
+        return self.get(self._size-1)
+
+    def set(self, index, e):
+        if index < 0 or index >= self._size:
+            raise IllegalArgumentException("set failed, illegal index.")
+        cur = self._dummy_head.next
+        for _ in range(index):
+            cur = cur.next
+        cur.val = e
+
+    def contains(self, e):
+        cur = self._dummy_head.next
+        while cur is not None:
+            if cur.val == e:
+                return True
+            cur = cur.next
+        return False
+    
+    def remove(self, index):
+        if index < 0 or index >= self._size:
+            raise IllegalArgumentException("remove failed, illegal index.")
+        prev = self._dummy_head
+        for _ in range(index):
+            prev = prev.next
+        ret = prev.next
+        prev.next = ret.next
+        self._size -= 1
+        return ret.val
+
+    def remove_first(self):
+        self.remove(0)
+
+    def remove_last(self):
+        self.remove(self._size-1)
+
+    def __str__(self):
+        res = ""
+        cur = self._dummy_head.next
+        while cur is not None:
+            res += str(cur.val) + "->"
+            cur = cur.next
+        res += "NULL"
+        return res
+    
+    def __repr__(self):
+        return self.__str__()
---
+++ tests/test_linked_list.py
@@ -0,0 +1,57 @@
+import unittest
+from serendipity.linear_structures.linked_list import LinkedList, IllegalArgumentException
+
+
+class LinkedListTestCase(unittest.TestCase):
+    def test_add_size_empty_get_contains(self):
+        linked = LinkedList()
+        try:
+            linked.add(2, 4)
+        except IllegalArgumentException:
+            assert True
+        else:
+            assert False
+        linked.add_first(1)
+        linked.add_last(2)
+        linked.add(1, 3)
+        self.assertEqual(linked.get_size(), 3)
+        self.assertFalse(linked.is_empty())
+        try:
+            linked.get(6)
+        except IllegalArgumentException:
+            assert True
+        else:
+            assert False
+
+        self.assertEqual(linked.get_first(), 1)
+        assert linked.get(1), 3
+        assert linked.get_last(), 2
+
+        self.assertFalse(linked.contains(6))
+        self.assertTrue(linked.contains(2))
+
+        try:
+            linked.set(5, 3)
+        except IllegalArgumentException:
+            assert True
+        else:
+            assert False
+
+        linked.set(2, 6)
+        self.assertEqual(linked.__str__(), "1->3->6->NULL")
+        self.assertEqual(linked.__repr__(), "1->3->6->NULL")
+
+        try:
+            linked.remove(6)
+        except IllegalArgumentException:
+            assert True
+        else:
+            assert False
+        
+        linked.remove_last()
+        self.assertEqual(linked.__str__(), "1->3->NULL")
+        self.assertEqual(linked.__repr__(), "1->3->NULL")
+
+        linked.remove_first()
+        self.assertEqual(linked.__str__(), "3->NULL")
+        self.assertEqual(linked.__repr__(), "3->NULL")
