be043bcabb3ad59aab0401c1839e7e4b32487305
Zhaowei <zwfangio@hotmail.com>
2019-01-16 15:01:24 +0800

    impl map with linked list

---
+++ tests/test_singly_linked_list_map.py
@@ -0,0 +1,19 @@
+import unittest
+from serendipity.set_and_map.singly_linked_list_map import Map
+
+
+class MapTestCase(unittest.TestCase):
+    def setUp(self):
+        self.map = Map()
+
+    def test_map(self):
+        self.map.add("a", 65)
+        self.assertEqual(self.map.get_size(), 1)
+        self.assertFalse(self.map.is_empty())
+        self.assertTrue(self.map.contains("a"))
+        self.map.set("a", 66)
+        self.assertTrue(self.map.contains("a"))
+        self.map.add("b", 23)
+        self.map.add("c", 22)
+        self.assertEqual(self.map.remove("b"), 23)
+        self.assertIsNone(self.map.remove("n"))
